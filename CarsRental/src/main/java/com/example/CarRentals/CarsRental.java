f1aa258a7e5556b374b8f088504b62682cf9e4b9
Atul Mishra <atulmishra1996@gmail.com>
2024-09-15 20:38:11 +0530

    adding mvn and spring

---
+++ CarsRental/src/main/java/com/example/CarRentals/CarsRental.java
@@ -0,0 +1,13 @@
+package com.example.CarRentals;
+
+/**
+ * Hello world!
+ *
+ */
+public class CarsRental 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
